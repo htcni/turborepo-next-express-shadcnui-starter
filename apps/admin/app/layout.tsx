40a29e4b31334940b68ed7f2418577bf945ba281
Nitin Bartwal <nitinbartwal6@gmail.com>
2023-08-03 23:03:35 +0530

    chore: config ui in web & add

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,11 +1,13 @@
+import 'ui/styles/global.css'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
